18af6ed67c74975a7b614a5a3d0725fd9b196880
Léo <leogianfagna@gmail.com>
2025-07-26 12:03:31 -0300

    feat: Estrutura de caminhos no vite.config

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,41 @@
 import express from 'express';
 import cors from 'cors';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-// Rota de teste
 app.get('/api/ping', (req, res) => {
   res.json({ message: 'test' });
 });
 
-app.listen(PORT, () => {
-  console.log(`Backend rodando na porta ${PORT}`);
+// WebSocket
+io.on('connection', (socket) => {
+  console.log('🟢 Novo cliente conectado', socket.id);
+
+  socket.on('mensagem', (data) => {
+    console.log('📨 Mensagem recebida:', data);
+    io.emit('mensagemRecebida', data); // envia para todos
+  });
+
+  socket.on('disconnect', () => {
+    console.log('🔴 Cliente desconectado', socket.id);
+  });
+});
+
+// Não usa app.listen
+httpServer.listen(PORT, () => {
+  console.log(`Servidor com WebSocket rodando na porta ${PORT}`);
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { io } from "socket.io-client";
+const socket = io("https://ludo-b5y7.onrender.com");
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [socketRes, setSocketRes] = useState("Aguardando...");
+  const handleSocket = () => {
+    socket.emit("mensagem", "Resposta do front");
+  };
+
+  useEffect(() => {
+    socket.on("mensagemRecebida", (data) => {
+      setSocketRes("Socket recebido.");
+    });
+
+    return () => socket.off("mensagemRecebida");
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={handleSocket}>Testar socket</button>
+      <p>{socketRes}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
